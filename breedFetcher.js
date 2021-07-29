a8d2790ec181682eeac608425bd4a514076b8faa
Dianne <mdianne.gabriel@gmail.com>
2021-07-29 15:42:19 -0400

    Refactored code. Used asynchronous flow. Eslint.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,24 @@
-const args = process.argv;
 const request = require('request');
 
-const breed = args[2];
+// const breed = args[2];
 const url = "https://api.thecatapi.com/v1/breeds/search?q=";
 // console.log(breed)
 
-request(url + breed, 'utf8', (error, response, body) => {
-  if (error) {
-    return console.log("Error: Invalid URL");
-  }
-  const data = JSON.parse(body);
-    
-  if (data[0] === undefined) {
-    throw Error(`${breed} is not found!`);
-  }
+const fetchBreedDescription = function(breed, callback) {
+  request(url + breed, 'utf8', (error, response, body) => {
+    if (error) {
+      // return console.log("Error: Invalid URL");
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+      
+    if (data[0] === undefined) {
+      // throw Error(`${breed} is not found!`);
+      callback(`${breed} was not found!`, null);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
 
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
