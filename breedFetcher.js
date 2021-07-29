cc67c42cd6af9bc32a6fa1ff62b462e485c3bc75
Dianne <mdianne.gabriel@gmail.com>
2021-07-29 15:19:31 -0400

    Updated

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,18 +2,18 @@ const args = process.argv;
 const request = require('request');
 
 const breed = args[2];
-const url = "https://api.thecatapi.co/v1/breeds/search?q=";
+const url = "https://api.thecatapi.com/v1/breeds/search?q=";
 // console.log(breed)
 
-  request(url+breed, 'utf8', (error, response, body) => {
-    if (error) {
-      return console.log("Error: Invalid URL")
-    }
-    const data = JSON.parse(body);
+request(url + breed, 'utf8', (error, response, body) => {
+  if (error) {
+    return console.log("Error: Invalid URL");
+  }
+  const data = JSON.parse(body);
     
-    if (data[0] === undefined) {
-      throw Error(`${breed} is not found!`)
-    }
+  if (data[0] === undefined) {
+    throw Error(`${breed} is not found!`);
+  }
 
-    console.log(data[0].description)
-  });
+  console.log(data[0].description);
+});
